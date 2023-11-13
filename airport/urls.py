e7e638fdde31c3f324f44504e2a612d63ae82bbe
halyna <1208petrova@gmail.com>
2023-11-13 12:19:05 +0200

    Register airports endpoints in airport/urls.py

---
+++ airport/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from airport.views import (
+    AirportViewSet,
+    AirplaneTypeViewSet,
+    AirplaneViewSet,
+    CrewViewSet,
+    FacilityViewSet,
+    FlightViewSet,
+    OrderViewSet,
+    RouteViewSet,
+)
+
+router = routers.DefaultRouter()
+router.register("airports", AirportViewSet)
+router.register("airplane_types", AirplaneTypeViewSet)
+router.register("airplanes", AirplaneViewSet)
+router.register("crews", CrewViewSet)
+router.register("facilities", FacilityViewSet)
+router.register("flights", FlightViewSet)
+router.register("orders", OrderViewSet)
+router.register("routes", RouteViewSet)
+
+urlpatterns = [path("", include(router.urls))]
+app_name = "airport"
